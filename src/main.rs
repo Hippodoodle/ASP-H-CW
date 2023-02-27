b7b36b7b1424ea04f62af653839879dc271fb63e
Thomas McCausland <thomas.mccausland11@gmail.com>
2023-02-27 17:57:32 +0000

    Finish dnslookup function

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,19 @@ use std::net::ToSocketAddrs;
 
 
 fn dnslookup(domains: Vec<String>) {
-
-    for mut domain in domains {
-        domain.push_str(":80");
-        match domain.to_socket_addrs() {
+    for domain in domains {
+        let domain_with_port = format!("{domain}:80");
+        match domain_with_port.to_socket_addrs() {
             Ok(addrs) => {
                 for addr in addrs {
-                    println!("{} {}", domain, addr)
+                    match addr {
+                        std::net::SocketAddr::V4(addr) => {
+                            println!("{} IPv4 {}", domain, addr.ip())
+                        }
+                        std::net::SocketAddr::V6(addr) => {
+                            println!("{} IPv6 {}", domain, addr.ip())
+                        }
+                    }
                 }
             }
             Err(e) => {
@@ -21,8 +27,6 @@ fn dnslookup(domains: Vec<String>) {
 
 
 fn main() {
-
     let args: Vec<String> = env::args().skip(1).collect();
     dnslookup(args);
-
 }
