382d1a04b62a042ad411d22352fe59b6d03c7fb8
Thomas McCausland <thomas.mccausland11@gmail.com>
2023-02-28 10:57:57 +0000

    Finished seqcon  function

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,53 @@
 use std::env;
-use std::net::ToSocketAddrs;
+use std::io::Write;
+use std::net::{ToSocketAddrs, SocketAddr, TcpStream};
 
 
-fn dnslookup(domains: Vec<String>) {
-    for domain in domains {
+fn dnslookup(domains: &Vec<String>) -> Result<(), std::io::Error> {
+    for domain in domains.iter() {
         let domain_with_port = format!("{domain}:80");
-        match domain_with_port.to_socket_addrs() {
-            Ok(addrs) => {
-                for addr in addrs {
-                    match addr {
-                        std::net::SocketAddr::V4(addr) => {
-                            println!("{} IPv4 {}", domain, addr.ip())
-                        }
-                        std::net::SocketAddr::V6(addr) => {
-                            println!("{} IPv6 {}", domain, addr.ip())
-                        }
-                    }
+
+        let addrs = domain_with_port.to_socket_addrs()?;
+
+        for addr in addrs.into_iter() {
+            match addr {
+                SocketAddr::V4(addr) => {
+                    println!("{domain} IPv4 {}", addr.ip())
                 }
-            }
-            Err(e) => {
-                println!("Error: {} {}", e, domain)
+                SocketAddr::V6(addr) => {
+                    println!("{domain} IPv6 {}", addr.ip())
+                }
             }
         }
     }
+
+    Ok(())
+}
+
+
+fn seqcon(domain: &String) -> Result<(), std::io::Error> {
+    let domain_with_port = format!("{domain}:80");
+
+    let addrs = domain_with_port.to_socket_addrs()?;
+
+    for addr in addrs.into_iter() {
+        if let Ok(mut stream) = TcpStream::connect(addr) {
+            println!("Connected on {}", addr.ip());
+            stream.write(format!("GET / HTTP/1.1\r\nHost: {domain}\r\n\r\n").as_bytes())?;
+            return Ok(())
+        }
+    }
+
+    println!("Couldn't connect to server...");
+    Ok(())
 }
 
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
-    dnslookup(args);
+    dnslookup(&args).expect("Error: Invalid socket address");
+
+    for domain in args.iter() {
+        seqcon(domain).expect("Error: Invalid socket address");
+    }
 }
